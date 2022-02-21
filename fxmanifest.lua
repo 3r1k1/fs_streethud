3feb89a4ad51d8ce93c0481959083c99d7941af5
LittleFishy <89689629+LittleFishyy@users.noreply.github.com>
2022-02-21 14:15:14 +0100

    Added new update, version 1.1

---
+++ fxmanifest.lua
@@ -0,0 +1,24 @@
+fx_version 'cerulean'
+
+game 'gta5'
+
+author 'LittleFishy#0001'
+
+lua54 'yes'
+
+shared_scripts {
+    'cfg.lua'
+}
+
+client_scripts {
+    'client/*.lua'
+}
+
+ui_page 'html/index.html'
+
+files {
+    'html/*.html',
+    'html/js/*.js',
+    'html/css/*.css',
+    'html/fonts/*.ttf'
+}
